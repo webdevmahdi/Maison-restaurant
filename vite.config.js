433587ca2ed43f04bf5f8687734beb85d34996b0
webdevmahdi <mahdihasan01705259@gmail.com>
2026-09-24 16:53:23 +0600

    check the error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/Maison-restaurant',
+  // base: env.VITE_BASE_PATH || '/Maison-restaurant',
 })
